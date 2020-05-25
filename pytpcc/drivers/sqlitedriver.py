847ae29210d209cd350249a7de0c4d70a0e7de97
Johann Schleier-Smith <jssmith@gmail.com>
2020-05-25 19:10:34 +0000

    attach as needed

--- pytpcc/drivers/sqlitedriver.py
+++ pytpcc/drivers/sqlitedriver.py
@@ -52,55 +52,71 @@ SQL_STOCKCOUNT = """
         AND S_QUANTITY < ?
 """
 
-def execQuery(c, query, args):
-    logging.debug("run query %s with args %s", query, str(args))
-    c.execute(query, args)
+activated_partitions = set()
+global_database = ""
+
+def attach(c, p):
+    global activated_partitions
+    dbname = global_database % p
+    if not p in activated_partitions:
+        logging.debug("Attach to database %s" % dbname)
+        c.execute("ATTACH DATABASE '%s' AS P%d" % (dbname, p))
+        activated_partitions.add(p)
+    else:
+        logging.debug("Already attached to databae %s" % dbname)
+
+def execQuery(c, query, partitions, args):
+    sql = query % partitions
+    logging.debug("run query %s with args %s", sql, str(args))
+    for p in partitions:
+        attach(c, p)
+    c.execute(sql, args)
 
 TXN_QUERIES = {
     "DELIVERY": {
-        "getNewOrder": lambda c, no_d_id, no_w_id: execQuery(c, "SELECT NO_O_ID FROM P%d.NEW_ORDER WHERE NO_D_ID = ? AND NO_W_ID = ? AND NO_O_ID > -1 LIMIT 1" % no_w_id, [no_d_id, no_w_id]),
-        "deleteNewOrder": lambda c, no_d_id, no_w_id, no_o_id: execQuery(c, "DELETE FROM P%d.NEW_ORDER WHERE NO_D_ID = ? AND NO_W_ID = ? AND NO_O_ID = ?" % no_w_id, [no_d_id, no_w_id, no_o_id]),
-        "getCId": lambda c, no_o_id, d_id, w_id: execQuery(c, "SELECT O_C_ID FROM P%d.ORDERS WHERE O_ID = ? AND O_D_ID = ? AND O_W_ID = ?" % w_id, [no_o_id, d_id, w_id]),
-        "updateOrders": lambda c, o_carrier_id, no_o_id, d_id, w_id: execQuery(c, "UPDATE P%d.ORDERS SET O_CARRIER_ID = ? WHERE O_ID = ? AND O_D_ID = ? AND O_W_ID = ?" % w_id, [o_carrier_id, no_o_id, d_id, w_id]),
-        "updateOrderLine": lambda c, o_entry_d, no_o_id, d_id, w_id: execQuery(c, "UPDATE P%d.ORDER_LINE SET OL_DELIVERY_D = ? WHERE OL_O_ID = ? AND OL_D_ID = ? AND OL_W_ID = ?" % w_id, [o_entry_d, no_o_id, d_id, w_id]),
-        "sumOLAmount": lambda c, no_o_id, d_id, w_id: execQuery(c, "SELECT SUM(OL_AMOUNT) FROM P%d.ORDER_LINE WHERE OL_O_ID = ? AND OL_D_ID = ? AND OL_W_ID = ?" % w_id, [no_o_id, d_id, w_id]),
-        "updateCustomer": lambda c, ol_total, c_id, d_id, w_id: execQuery(c, "UPDATE P%d.CUSTOMER SET C_BALANCE = C_BALANCE + ? WHERE C_ID = ? AND C_D_ID = ? AND C_W_ID = ?" % w_id, [ol_total, c_id, d_id, w_id]),
+        "getNewOrder": lambda c, no_d_id, no_w_id: execQuery(c, "SELECT NO_O_ID FROM P%d.NEW_ORDER WHERE NO_D_ID = ? AND NO_W_ID = ? AND NO_O_ID > -1 LIMIT 1", (no_w_id,), [no_d_id, no_w_id]),
+        "deleteNewOrder": lambda c, no_d_id, no_w_id, no_o_id: execQuery(c, "DELETE FROM P%d.NEW_ORDER WHERE NO_D_ID = ? AND NO_W_ID = ? AND NO_O_ID = ?", (no_w_id,), [no_d_id, no_w_id, no_o_id]),
+        "getCId": lambda c, no_o_id, d_id, w_id: execQuery(c, "SELECT O_C_ID FROM P%d.ORDERS WHERE O_ID = ? AND O_D_ID = ? AND O_W_ID = ?", (w_id,), [no_o_id, d_id, w_id]),
+        "updateOrders": lambda c, o_carrier_id, no_o_id, d_id, w_id: execQuery(c, "UPDATE P%d.ORDERS SET O_CARRIER_ID = ? WHERE O_ID = ? AND O_D_ID = ? AND O_W_ID = ?", (w_id,), [o_carrier_id, no_o_id, d_id, w_id]),
+        "updateOrderLine": lambda c, o_entry_d, no_o_id, d_id, w_id: execQuery(c, "UPDATE P%d.ORDER_LINE SET OL_DELIVERY_D = ? WHERE OL_O_ID = ? AND OL_D_ID = ? AND OL_W_ID = ?", (w_id,), [o_entry_d, no_o_id, d_id, w_id]),
+        "sumOLAmount": lambda c, no_o_id, d_id, w_id: execQuery(c, "SELECT SUM(OL_AMOUNT) FROM P%d.ORDER_LINE WHERE OL_O_ID = ? AND OL_D_ID = ? AND OL_W_ID = ?", (w_id,), [no_o_id, d_id, w_id]),
+        "updateCustomer": lambda c, ol_total, c_id, d_id, w_id: execQuery(c, "UPDATE P%d.CUSTOMER SET C_BALANCE = C_BALANCE + ? WHERE C_ID = ? AND C_D_ID = ? AND C_W_ID = ?", (w_id,), [ol_total, c_id, d_id, w_id]),
     },
     "NEW_ORDER": {
-        "getWarehouseTaxRate": lambda c, w_id: execQuery(c, "SELECT W_TAX FROM P%d.WAREHOUSE WHERE W_ID = ?" % w_id, [w_id]),
-        "getDistrict": lambda c, d_id, w_id: execQuery(c, "SELECT D_TAX, D_NEXT_O_ID FROM P%d.DISTRICT WHERE D_ID = ? AND D_W_ID = ?" % w_id, [d_id, w_id]),
-        "incrementNextOrderId": lambda c, d_next_o_id, d_id, w_id: execQuery(c, "UPDATE P%d.DISTRICT SET D_NEXT_O_ID = ? WHERE D_ID = ? AND D_W_ID = ?" % w_id, [d_next_o_id, d_id, w_id]),
-        "getCustomer": lambda c, w_id, d_id, c_id: execQuery(c, "SELECT C_DISCOUNT, C_LAST, C_CREDIT FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?" % w_id, [w_id, d_id, c_id]),
-        "createOrder": lambda c, d_next_o_id, d_id, w_id, c_id, o_entry_d, o_carrier_id, o_ol_cnt, o_all_local: execQuery(c, "INSERT INTO P%d.ORDERS (O_ID, O_D_ID, O_W_ID, O_C_ID, O_ENTRY_D, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL) VALUES (?, ?, ?, ?, ?, ?, ?, ?)" % w_id, [d_next_o_id, d_id, w_id, c_id, o_entry_d, o_carrier_id, o_ol_cnt, o_all_local]),
-        "createNewOrder": lambda c, o_id, d_id, w_id: execQuery(c, "INSERT INTO P%d.NEW_ORDER (NO_O_ID, NO_D_ID, NO_W_ID) VALUES (?, ?, ?)" % w_id, [o_id, d_id, w_id]),
-        "getItemInfo": lambda c, w_id, ol_i_id: execQuery(c, "SELECT I_PRICE, I_NAME, I_DATA FROM P%d.ITEM WHERE I_ID = ?" % w_id, [ol_i_id]),
-        "getStockInfo": lambda c, d_id, ol_i_id, ol_supply_w_id: execQuery(c, "SELECT S_QUANTITY, S_DATA, S_YTD, S_ORDER_CNT, S_REMOTE_CNT, S_DIST_%02d FROM P%d.STOCK WHERE S_I_ID = ? AND S_W_ID = ?" % (d_id, ol_supply_w_id), [ol_i_id, ol_supply_w_id]),
-        "updateStock": lambda c, s_quantity, s_ytd, s_order_cnt, s_remote_cnt, ol_i_id, ol_supply_w_id: execQuery(c, "UPDATE P%d.STOCK SET S_QUANTITY = ?, S_YTD = ?, S_ORDER_CNT = ?, S_REMOTE_CNT = ? WHERE S_I_ID = ? AND S_W_ID = ?" % ol_supply_w_id, [s_quantity, s_ytd, s_order_cnt, s_remote_cnt, ol_i_id, ol_supply_w_id]),
-        "createOrderLine": lambda c, d_next_o_id, d_id, w_id, ol_number, ol_i_id, ol_supply_w_id, o_entry_d, ol_quantity, ol_amount, s_dist_xx: execQuery(c, "INSERT INTO P%d.ORDER_LINE (OL_O_ID, OL_D_ID, OL_W_ID, OL_NUMBER, OL_I_ID, OL_SUPPLY_W_ID, OL_DELIVERY_D, OL_QUANTITY, OL_AMOUNT, OL_DIST_INFO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" % w_id, [d_next_o_id, d_id, w_id, ol_number, ol_i_id, ol_supply_w_id, o_entry_d, ol_quantity, ol_amount, s_dist_xx]),
+        "getWarehouseTaxRate": lambda c, w_id: execQuery(c, "SELECT W_TAX FROM P%d.WAREHOUSE WHERE W_ID = ?", (w_id,), [w_id]),
+        "getDistrict": lambda c, d_id, w_id: execQuery(c, "SELECT D_TAX, D_NEXT_O_ID FROM P%d.DISTRICT WHERE D_ID = ? AND D_W_ID = ?", (w_id,), [d_id, w_id]),
+        "incrementNextOrderId": lambda c, d_next_o_id, d_id, w_id: execQuery(c, "UPDATE P%d.DISTRICT SET D_NEXT_O_ID = ? WHERE D_ID = ? AND D_W_ID = ?", (w_id,), [d_next_o_id, d_id, w_id]),
+        "getCustomer": lambda c, w_id, d_id, c_id: execQuery(c, "SELECT C_DISCOUNT, C_LAST, C_CREDIT FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?", (w_id,), [w_id, d_id, c_id]),
+        "createOrder": lambda c, d_next_o_id, d_id, w_id, c_id, o_entry_d, o_carrier_id, o_ol_cnt, o_all_local: execQuery(c, "INSERT INTO P%d.ORDERS (O_ID, O_D_ID, O_W_ID, O_C_ID, O_ENTRY_D, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (w_id,), [d_next_o_id, d_id, w_id, c_id, o_entry_d, o_carrier_id, o_ol_cnt, o_all_local]),
+        "createNewOrder": lambda c, o_id, d_id, w_id: execQuery(c, "INSERT INTO P%d.NEW_ORDER (NO_O_ID, NO_D_ID, NO_W_ID) VALUES (?, ?, ?)", (w_id,), [o_id, d_id, w_id]),
+        "getItemInfo": lambda c, w_id, ol_i_id: execQuery(c, "SELECT I_PRICE, I_NAME, I_DATA FROM P%d.ITEM WHERE I_ID = ?", (w_id,), [ol_i_id]),
+        "getStockInfo": lambda c, d_id, ol_i_id, ol_supply_w_id: execQuery(c, "SELECT S_QUANTITY, S_DATA, S_YTD, S_ORDER_CNT, S_REMOTE_CNT, S_DIST_%02d FROM P%%d.STOCK WHERE S_I_ID = ? AND S_W_ID = ?" % d_id, (ol_supply_w_id,), [ol_i_id, ol_supply_w_id]),
+        "updateStock": lambda c, s_quantity, s_ytd, s_order_cnt, s_remote_cnt, ol_i_id, ol_supply_w_id: execQuery(c, "UPDATE P%d.STOCK SET S_QUANTITY = ?, S_YTD = ?, S_ORDER_CNT = ?, S_REMOTE_CNT = ? WHERE S_I_ID = ? AND S_W_ID = ?", (ol_supply_w_id,), [s_quantity, s_ytd, s_order_cnt, s_remote_cnt, ol_i_id, ol_supply_w_id]),
+        "createOrderLine": lambda c, d_next_o_id, d_id, w_id, ol_number, ol_i_id, ol_supply_w_id, o_entry_d, ol_quantity, ol_amount, s_dist_xx: execQuery(c, "INSERT INTO P%d.ORDER_LINE (OL_O_ID, OL_D_ID, OL_W_ID, OL_NUMBER, OL_I_ID, OL_SUPPLY_W_ID, OL_DELIVERY_D, OL_QUANTITY, OL_AMOUNT, OL_DIST_INFO) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (w_id,), [d_next_o_id, d_id, w_id, ol_number, ol_i_id, ol_supply_w_id, o_entry_d, ol_quantity, ol_amount, s_dist_xx]),
     },
     
     "ORDER_STATUS": {
-        "getCustomerByCustomerId": lambda c, w_id, d_id, c_id: execQuery(c, "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_BALANCE FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?" % w_id, [w_id, d_id, c_id]),
-        "getCustomersByLastName": lambda c, w_id, d_id, c_last: execQuery(c, "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_BALANCE FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_LAST = ? ORDER BY C_FIRST" % w_id, [w_id, d_id, c_last]),
-        "getLastOrder": lambda c, w_id, d_id, c_id: execQuery(c, "SELECT O_ID, O_CARRIER_ID, O_ENTRY_D FROM P%d.ORDERS WHERE O_W_ID = ? AND O_D_ID = ? AND O_C_ID = ? ORDER BY O_ID DESC LIMIT 1" % w_id, [w_id, d_id, c_id]),
-        "getOrderLines": lambda c, w_id, d_id, o_id: execQuery(c, "SELECT OL_SUPPLY_W_ID, OL_I_ID, OL_QUANTITY, OL_AMOUNT, OL_DELIVERY_D FROM P%d.ORDER_LINE WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?" % w_id, [w_id, d_id, o_id]),
+        "getCustomerByCustomerId": lambda c, w_id, d_id, c_id: execQuery(c, "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_BALANCE FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?", (w_id,), [w_id, d_id, c_id]),
+        "getCustomersByLastName": lambda c, w_id, d_id, c_last: execQuery(c, "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_BALANCE FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_LAST = ? ORDER BY C_FIRST", (w_id,), [w_id, d_id, c_last]),
+        "getLastOrder": lambda c, w_id, d_id, c_id: execQuery(c, "SELECT O_ID, O_CARRIER_ID, O_ENTRY_D FROM P%d.ORDERS WHERE O_W_ID = ? AND O_D_ID = ? AND O_C_ID = ? ORDER BY O_ID DESC LIMIT 1", (w_id,), [w_id, d_id, c_id]),
+        "getOrderLines": lambda c, w_id, d_id, o_id: execQuery(c, "SELECT OL_SUPPLY_W_ID, OL_I_ID, OL_QUANTITY, OL_AMOUNT, OL_DELIVERY_D FROM P%d.ORDER_LINE WHERE OL_W_ID = ? AND OL_D_ID = ? AND OL_O_ID = ?", (w_id,), [w_id, d_id, o_id]),
     },
     
     "PAYMENT": {
-        "getWarehouse": lambda c, w_id: execQuery(c, "SELECT W_NAME, W_STREET_1, W_STREET_2, W_CITY, W_STATE, W_ZIP FROM P%d.WAREHOUSE WHERE W_ID = ?" % w_id, [w_id]),
-        "updateWarehouseBalance": lambda c, h_amount, w_id: execQuery(c, "UPDATE P%d.WAREHOUSE SET W_YTD = W_YTD + ? WHERE W_ID = ?" % w_id, [h_amount, w_id]),
-        "getDistrict": lambda c, w_id, d_id: execQuery(c, "SELECT D_NAME, D_STREET_1, D_STREET_2, D_CITY, D_STATE, D_ZIP FROM P%d.DISTRICT WHERE D_W_ID = ? AND D_ID = ?" % w_id, [w_id, d_id]),
-        "updateDistrictBalance": lambda c, h_amount, d_w_id, d_id: execQuery(c, "UPDATE P%d.DISTRICT SET D_YTD = D_YTD + ? WHERE D_W_ID  = ? AND D_ID = ?" % d_w_id, [h_amount, d_w_id, d_id]),
-        "getCustomerByCustomerId": lambda c, w_id, d_id, c_id: execQuery(c, "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP, C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT, C_BALANCE, C_YTD_PAYMENT, C_PAYMENT_CNT, C_DATA FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?" % w_id, [w_id, d_id, c_id]),
-        "getCustomersByLastName": lambda c, w_id, d_id, c_last: execQuery(c, "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP, C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT, C_BALANCE, C_YTD_PAYMENT, C_PAYMENT_CNT, C_DATA FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_LAST = ? ORDER BY C_FIRST" % w_id, [w_id, d_id, c_last]),
-        "updateBCCustomer": lambda c, c_balance, c_ytd_payment, c_payment_cnt, c_data, c_w_id, c_d_id, c_id: execQuery(c, "UPDATE P%d.CUSTOMER SET C_BALANCE = ?, C_YTD_PAYMENT = ?, C_PAYMENT_CNT = ?, C_DATA = ? WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?" % c_w_id, [c_balance, c_ytd_payment, c_payment_cnt, c_data, c_w_id, c_d_id, c_id]),
-        "updateGCCustomer": lambda c, c_balance, c_ytd_payment, c_payment_cnt, c_w_id, c_d_id, c_id: execQuery(c, "UPDATE P%d.CUSTOMER SET C_BALANCE = ?, C_YTD_PAYMENT = ?, C_PAYMENT_CNT = ? WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?" % c_w_id, [c_balance, c_ytd_payment, c_payment_cnt, c_w_id, c_d_id, c_id]),
-        "insertHistory": lambda c, h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id, h_date, h_amount, h_data: execQuery(c, "INSERT INTO P%d.HISTORY VALUES (?, ?, ?, ?, ?, ?, ?, ?)" % h_w_id, [h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id, h_date, h_amount, h_data]),
+        "getWarehouse": lambda c, w_id: execQuery(c, "SELECT W_NAME, W_STREET_1, W_STREET_2, W_CITY, W_STATE, W_ZIP FROM P%d.WAREHOUSE WHERE W_ID = ?", (w_id,), [w_id]),
+        "updateWarehouseBalance": lambda c, h_amount, w_id: execQuery(c, "UPDATE P%d.WAREHOUSE SET W_YTD = W_YTD + ? WHERE W_ID = ?", (w_id,), [h_amount, w_id]),
+        "getDistrict": lambda c, w_id, d_id: execQuery(c, "SELECT D_NAME, D_STREET_1, D_STREET_2, D_CITY, D_STATE, D_ZIP FROM P%d.DISTRICT WHERE D_W_ID = ? AND D_ID = ?", (w_id,), [w_id, d_id]),
+        "updateDistrictBalance": lambda c, h_amount, d_w_id, d_id: execQuery(c, "UPDATE P%d.DISTRICT SET D_YTD = D_YTD + ? WHERE D_W_ID  = ? AND D_ID = ?", (d_w_id,), [h_amount, d_w_id, d_id]),
+        "getCustomerByCustomerId": lambda c, w_id, d_id, c_id: execQuery(c, "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP, C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT, C_BALANCE, C_YTD_PAYMENT, C_PAYMENT_CNT, C_DATA FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?", (w_id,), [w_id, d_id, c_id]),
+        "getCustomersByLastName": lambda c, w_id, d_id, c_last: execQuery(c, "SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST, C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP, C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT, C_BALANCE, C_YTD_PAYMENT, C_PAYMENT_CNT, C_DATA FROM P%d.CUSTOMER WHERE C_W_ID = ? AND C_D_ID = ? AND C_LAST = ? ORDER BY C_FIRST", (w_id,), [w_id, d_id, c_last]),
+        "updateBCCustomer": lambda c, c_balance, c_ytd_payment, c_payment_cnt, c_data, c_w_id, c_d_id, c_id: execQuery(c, "UPDATE P%d.CUSTOMER SET C_BALANCE = ?, C_YTD_PAYMENT = ?, C_PAYMENT_CNT = ?, C_DATA = ? WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?", (c_w_id,), [c_balance, c_ytd_payment, c_payment_cnt, c_data, c_w_id, c_d_id, c_id]),
+        "updateGCCustomer": lambda c, c_balance, c_ytd_payment, c_payment_cnt, c_w_id, c_d_id, c_id: execQuery(c, "UPDATE P%d.CUSTOMER SET C_BALANCE = ?, C_YTD_PAYMENT = ?, C_PAYMENT_CNT = ? WHERE C_W_ID = ? AND C_D_ID = ? AND C_ID = ?", (c_w_id,), [c_balance, c_ytd_payment, c_payment_cnt, c_w_id, c_d_id, c_id]),
+        "insertHistory": lambda c, h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id, h_date, h_amount, h_data: execQuery(c, "INSERT INTO P%d.HISTORY VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (h_w_id,), [h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id, h_date, h_amount, h_data]),
     },
     
     "STOCK_LEVEL": {
-        "getOId": lambda c, d_w_id, d_id: execQuery(c, "SELECT D_NEXT_O_ID FROM P%d.DISTRICT WHERE D_W_ID = ? AND D_ID = ?" % d_w_id, [d_w_id, d_id]),
-        "getStockCount": lambda c, ol_w_id, ol_d_id, ol_o_id_max, ol_o_id_min, s_w_id, s_quantity_max: execQuery(c, SQL_STOCKCOUNT % (ol_w_id, s_w_id), [ol_w_id, ol_d_id, ol_o_id_max, ol_o_id_min, s_w_id, s_quantity_max]),
+        "getOId": lambda c, d_w_id, d_id: execQuery(c, "SELECT D_NEXT_O_ID FROM P%d.DISTRICT WHERE D_W_ID = ? AND D_ID = ?", (d_w_id,), [d_w_id, d_id]),
+        "getStockCount": lambda c, ol_w_id, ol_d_id, ol_o_id_max, ol_o_id_min, s_w_id, s_quantity_max: execQuery(c, SQL_STOCKCOUNT, (ol_w_id, s_w_id), [ol_w_id, ol_d_id, ol_o_id_max, ol_o_id_min, s_w_id, s_quantity_max]),
     },
 }
 
@@ -137,6 +153,8 @@ class SqliteDriver(abstractdriver.AbstractDriver):
             assert key in config, "Missing parameter '%s' in %s configuration" % (key, self.name)
 
         self.database = str(config["database"])
+        global global_database
+        global_database = self.database
         self.vfs = str(config["vfs"]).lower()
         self.journal_mode = str(config["journal_mode"]).lower()
         self.locking_mode = str(config["locking_mode"]).lower()
@@ -145,23 +163,6 @@ class SqliteDriver(abstractdriver.AbstractDriver):
         self.conn = None
 
         self.setup()
-
-    def setup(self):
-        logging.debug("entering setup")
-        if self.conn:
-            logging.debug("connection exists so exiting")
-            return
-        logging.debug("setup new db connection")
-        self.conn = sqlite3.connect(":memory:")
-        self.cursor = self.conn.cursor()
-
-        try:
-            for i in range(1, 9):
-                dbname = self.database % i
-                logging.debug("Attach to database %s", dbname)
-                self.cursor.execute("ATTACH DATABASE '%s' AS P%d" % (dbname, i))
-        except sqlite3.OperationalError as err:
-            print(err)
 
         try:
             self.cursor.execute("PRAGMA cache_size")
@@ -188,8 +189,18 @@ class SqliteDriver(abstractdriver.AbstractDriver):
             print(err)
 
 
+    def setup(self):
+        logging.debug("entering setup")
+        if self.conn:
+            self.abort()
+        logging.debug("setup new db connection")
+        self.conn = sqlite3.connect(":memory:")
+        self.cursor = self.conn.cursor()
+        global activated_partitions
+        activated_partitions = set()
+
     def abort(self):
-        logging.debug("abort - shut db")
+        logging.debug("close connections")
         if self.cursor:
             try:
                 logging.debug("close down cursor")
@@ -283,6 +294,11 @@ class SqliteDriver(abstractdriver.AbstractDriver):
         i_ids = params["i_ids"]
         i_w_ids = params["i_w_ids"]
         i_qtys = params["i_qtys"]
+
+        need_partitions = set(i_w_ids)
+        need_partitions.add(w_id)
+        for p in need_partitions:
+            attach(self.cursor, p)
             
         assert len(i_ids) > 0
         assert len(i_ids) == len(i_w_ids)
